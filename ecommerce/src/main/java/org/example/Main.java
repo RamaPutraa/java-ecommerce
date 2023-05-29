329123f6bc920d8d135d97a3ce58211a4ff30a28
ramaputra <ramaputra221203@gmail.com>
2023-05-29 21:40:20 +0800

    build method GET

--- ecommerce/src/main/java/org/example/Main.java
+++ ecommerce/src/main/java/org/example/Main.java
@@ -1,37 +1,20 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import config.SqlConnection;
-
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 4127), 0);
-        httpServer.createContext("/ecommerce", new Ecommerce());
-        httpServer.setExecutor(Executors.newSingleThreadExecutor());
-        httpServer.start();
-    }
-
-    public static class Ecommerce implements HttpHandler {
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-
-            if("GET".equals(exchange.getRequestMethod())){
-                OutputStream outputStream = exchange.getResponseBody();
-                String responseToBeSentBack = SqlConnection.Connection();
-                exchange.sendResponseHeaders(200, responseToBeSentBack.length());
-
-                outputStream.write(responseToBeSentBack.getBytes());
-                outputStream.flush();
-                outputStream.close();
-            }
+        try{
+            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 8127),0);
+            httpserver.createContext("/", new Server());
+            httpserver.setExecutor(Executors.newSingleThreadExecutor());
+            httpserver.start();
+            System.out.println("Listening on port 8127...");
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
